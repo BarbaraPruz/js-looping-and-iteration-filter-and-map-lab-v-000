c429f06a9b71c425f5408fbbfae5fb1084c89119
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-08-28 01:17:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,11 @@ function driverNamesWithRevenueOver(drivers, revenue) {
 function exactMatch (drivers, matchObject) {
   key = Object.keys(matchObject)[0];
   value = matchObject[key];
+  console.log("drivers",drivers);
+  console.log("matchobj",matchObject);
+  console.log("Matching",key, value);
   return drivers.map ( function (driver) {
+    console.log("cb driver",driver,"key",driver[key]);
     driver[key] === value
   });
 }
